b4ad6f54ce13e7bee1a920abd4208c10c0d46a42
eugeneswalker <eugenesunsetwalker@gmail.com>
2023-04-10 08:01:55 -0700

    initial commit

---
+++ master/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BUILD_DATE=$(printf '%(%Y-%m-%d)T' -1)
+BUILD_TAG=${BUILD_TAG:-${BUILD_DATE}}
+
+REGISTRY=${REGISTRY:-esw123}
+OUTPUT_IMAGE="${REGISTRY}/buildbot-master:${BUILD_TAG}"
+
+docker build -t "${OUTPUT_IMAGE}" .
